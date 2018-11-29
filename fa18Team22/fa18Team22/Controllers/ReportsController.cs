3f5d8bad4298263f948bb5c19bff6f75fe18dfbd
Whitt Hyde <whyde@utexas.edu>
2018-11-29 09:46:57 -0600

    ReportsController

--- fa18Team22/fa18Team22/Controllers/ReportsController.cs
+++ fa18Team22/fa18Team22/Controllers/ReportsController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using fa18Team22.DAL;
+using fa18Team22.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +14,46 @@ namespace fa18Team22.Controllers
 {
     public class ReportsController : Controller
     {
+        private AppDbContext _db;
+        public ReportsController(AppDbContext context) { _db = context; }
+
         // GET: /<controller>/
         public IActionResult ChooseReport()
         {
             return View();
         }
+
+        // GET: /<controller>/
+        public IActionResult AllBooksSold()
+        {
+            ViewBag.SelectedRecords = _db.Books.Count();
+            return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult AllCustomers()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Totals()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult CurrentInventory()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult ApprovedRejectedReviews()
+        {
+            return View();
+        }
+
 
         // GET: /<controller>/
         public IActionResult Index()
