8102f831297cd10316199d7073775ebbee720d5a
James Albritton <jra3464@utexas.edu>
2018-11-29 23:09:42 -0600

    * AppUser.cs: * AppDbContextModelSnapshot.cs: * 20181130050507_ccerrormessage.Designer.cs:
    * 20181130050507_ccerrormessage.cs: Added error message for
      CreditCards

---
+++ fa18Team22/fa18Team22/Migrations/20181130050507_ccerrormessage.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fa18Team22.Migrations
+{
+    public partial class ccerrormessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreditCard3",
+                table: "AspNetUsers",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreditCard2",
+                table: "AspNetUsers",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreditCard1",
+                table: "AspNetUsers",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreditCard3",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreditCard2",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreditCard1",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 16,
+                oldNullable: true);
+        }
+    }
+}
