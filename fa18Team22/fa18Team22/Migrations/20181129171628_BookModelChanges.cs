1aa53a2cfb4c7fec5d8c715bf600652128879221
Meredith <meredithknight2016@gmail.com>
2018-11-29 21:53:44 -0600

    * Order.cs: * OrderDetail.cs: * Details.cshtml: * Edit.cshtml: * OrdersController.cs: * ShoppingCart.cshtml: * OrderDetailsController.cs: working on orders and adding to order
    * Book.cs:
    * AppDbContextModelSnapshot.cs:
    * 20181129171628_BookModelChanges.cs:
    * 20181129171628_BookModelChanges.Designer.cs: migrated to add book
      model avgCost

---
+++ fa18Team22/fa18Team22/Migrations/20181129171628_BookModelChanges.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fa18Team22.Migrations
+{
+    public partial class BookModelChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AvgBookCost",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvgBookCost",
+                table: "Books");
+        }
+    }
+}
