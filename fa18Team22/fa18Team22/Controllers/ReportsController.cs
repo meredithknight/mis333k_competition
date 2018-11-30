bfa5cf2e27df2afb44b223c4eb10c01274cb3fa3
Whitt Hyde <whyde@utexas.edu>
2018-11-29 21:34:07 -0600

    Push It Push it real good

--- fa18Team22/fa18Team22/Controllers/ReportsController.cs
+++ fa18Team22/fa18Team22/Controllers/ReportsController.cs
@@ -38,6 +38,7 @@ namespace fa18Team22.Controllers
             List<Book> SelectedBooks = new List<Book>();
 
             var query = from b in _db.Books select b;
+            SelectedBooks = query.Include(b => b.OrderDetails).ThenInclude(b => b.Order).ToList();
 
             decimal totalcost = 0;
             decimal totalprice = 0;
@@ -47,7 +48,7 @@ namespace fa18Team22.Controllers
 
             foreach(Book b in SelectedBooks)
             {
-                countervariable += countervariable;
+                countervariable += 1;
                 totalcost += b.BookCost;
                 totalprice += b.SalesPrice;
             }
@@ -58,7 +59,7 @@ namespace fa18Team22.Controllers
             foreach (Book b in SelectedBooks)
             {
                 b.AvgBookCost = avgcost;
-
+                //b.BookProfitMargin = b.SalesPrice - avgcost;
                 //TODO: Should we calculate Avg Sales price here. Intial Reaction no, because in the order details is where coupons and promos will be added and this will just take the latest price
             }
 
@@ -66,13 +67,14 @@ namespace fa18Team22.Controllers
 
             if (SelectedSort == SortReport.MostRecent)
             {
-                return View("AllBooksIndex", SelectedBooks.OrderBy(b => b.PublishDate));
+                return View("AllBooksIndex", SelectedBooks);
+                            //.OrderBy(b => b.PublishDate));
             }
-
-            if (SelectedSort == SortReport.ProfitMarginAsc)
-            {
-                return View("AllBooksIndex", SelectedBooks.OrderBy(b => b.))
-            }
+            return View();
+            //if (SelectedSort == SortReport.ProfitMarginAsc)
+            //{
+            //    return View("AllBooksIndex", SelectedBooks.OrderBy(b => b.))
+            //}
         }
 
         // GET: /<controller>/
--- fa18Team22/fa18Team22/Migrations/20181130021155_MigrationNov29a.Designer.cs
+++ fa18Team22/fa18Team22/Migrations/20181130021155_MigrationNov29a.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fa18Team22.DAL;
 
 namespace fa18Team22.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181130021155_MigrationNov29a")]
+    partial class MigrationNov29a
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
