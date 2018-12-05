f03ba4c6c10fe03034fb055761bc79f2e22b3495
Tommy Nguyen <TommyNguyen@wireless-10-146-241-130.public.utexas.edu>
2018-12-04 21:31:27 -0600

    stuff

--- fa18Team22/fa18Team22/Controllers/HomeController.cs
+++ fa18Team22/fa18Team22/Controllers/HomeController.cs
@@ -1,18 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using fa18Team22.DAL;
+using fa18Team22.Models;
+using fa18Team22.Utilities;
 
 namespace fa18Team22.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
-        {
-        }
 
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public async Task<IActionResult> Index()
         {
-            return View();
+            var query = from r in _context.Promos select r;
+            List<Promo> promos = query.ToList();
+
+
+            return View(promos);
         }
     }
 }
